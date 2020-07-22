e07c1fb2c8a9ae04690130f57edf28d9a2295603
Jonathan Reed <jreed@keytechinc.com>
2020-07-22 09:35:16 -0400

    Added files

---
+++ infer_underscore.py
@@ -0,0 +1,20 @@
+def infer_underscore(lines):
+    new_lines = []
+    operation_chars = "+-*/."
+    numeric_chars = "0123456789"
+    if not isinstance(lines, list):
+        lines = [lines]
+    for line in lines:
+        first_char = line[0]
+        second_char = line[1]
+        if first_char in operation_chars:
+            if first_char == ".":
+                if second_char in numeric_chars:
+                    new_lines.append(line)
+                    continue
+
+            new_line = "_"+line
+            new_lines.append(new_line)
+        else:
+            new_lines.append(line)
+    return new_lines
